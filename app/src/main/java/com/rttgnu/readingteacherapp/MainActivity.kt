c8a9a7b511e0b85ae074e47434f347950d6b5cbb
Valurauta <wncjf2000@naver.com>
2022-08-30 22:40:52 +0900

    1

--- app/src/main/java/com/rttgnu/readingteacherapp/MainActivity.kt
+++ app/src/main/java/com/rttgnu/readingteacherapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.rttgnu.readingteacherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val moveButton = findViewById<Button>(R.id.movetestButton)
+
+        // 페이지 이동
+        fun moveToAnotherPage() {
+            val intent = Intent(this, movetestButton::class.java)
+            startActivity(intent)
+        }
+  0
+        // 함수호출
+        moveButton.setOnClickListener {
+            moveToAnotherPage()
+        }
     }
 }
---
+++ app/src/main/java/com/rttgnu/readingteacherapp/movetestButton.kt
@@ -0,0 +1,11 @@
+package com.rttgnu.readingteacherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class movetestButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movetest_button)
+    }
+}
