ee4eb9250bb04e255be3d2c7b88a23310def0796
anima0314 <anima0314@naver.com>
2022-08-30 15:26:07 +0900

    라이브러리 클래스 메인화면 만듬

---
+++ app/src/main/java/com/rttgnu/readingteacherapp/libraryActivity.kt
@@ -0,0 +1,11 @@
+package com.rttgnu.readingteacherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class libraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
