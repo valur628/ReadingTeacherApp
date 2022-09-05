49dd52e04a58cdf075bd64118167a8de634aa51b
anima0314 <anima0314@naver.com>
2022-09-05 22:52:45 +0900

    구글 로그인 구현

--- app/src/main/java/com/rttgnu/readingteacherapp/LibraryActivity.kt
+++ app/src/main/java/com/rttgnu/readingteacherapp/LibraryActivity.kt
@@ -3,7 +3,7 @@ package com.rttgnu.readingteacherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class libraryActivity : AppCompatActivity() {
+class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
