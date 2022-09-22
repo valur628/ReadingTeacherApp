fb8052229cac9dacb2c0b77795855ae7979bef7a
anima0314 <anima0314@naver.com>
2022-09-23 00:02:53 +0900

    데이터 추가는 했지만 커스텀데이터를 불러오는데 오류

--- app/src/main/java/com/rttgnu/readingteacherapp/LibraryFragment.kt
+++ app/src/main/java/com/rttgnu/readingteacherapp/LibraryFragment.kt
@@ -1,36 +1,33 @@
 package com.rttgnu.readingteacherapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import com.rttgnu.readingteacherapp.databinding.FragmentLibraryBinding
 
 class LibraryFragment : Fragment() {
     // 바인딩 객체 타입에 ?를 붙여서 null을 허용 해줘야한다. ( onDestroy 될 때 완벽하게 제거를 하기위해 )
     lateinit var mBinding: FragmentLibraryBinding
     // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
+    var TAG = "Library"
+    val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View {
-        // 액티비티 와는 다르게 layoutInflater 를 쓰지 않고 inflater 인자를 가져와 뷰와 연결한다.
-        val LibraryList = arrayListOf(
-            LibraryModel(1,"눈의 여왕","한스 얀데르센",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"삼국지연의","나관중",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"해와 달이 된 오누이","작자 미상",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"성냥팔이 소녀","한스 얀데르센",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"브레멘 음악대","그림 형제",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"견우와 직녀","작자 미상",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"눈의 여왕","한스 얀데르센",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"삼국지연의","나관중",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"해와 달이 된 오누이","작자 미상",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"성냥팔이 소녀","한스 얀데르센",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"브레멘 음악대","그림 형제",R.drawable.book,0,1,"눈의여왕 설명"),
-            LibraryModel(1,"견우와 직녀","작자 미상",R.drawable.book,0,1,"눈의여왕 설명")
-        )
+        //변수
+        val LibraryList = arrayListOf<LibraryModel>()
+
+        db.collection("Library")
+            .whereEqualTo("libraryCategory", 1)
+            .get()
 
         //바인딩
         mBinding = FragmentLibraryBinding.inflate(inflater, container, false)
@@ -38,9 +35,9 @@ class LibraryFragment : Fragment() {
         //mBinding.library.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
         mBinding.rviewlibrary.setHasFixedSize(true)
 
-
         //어뎁터 연결
         mBinding.rviewlibrary.adapter = LibraryAdapter(LibraryList)
+        Log.d(TAG, "어댑터 연결?")
 
         return mBinding.root
     }
